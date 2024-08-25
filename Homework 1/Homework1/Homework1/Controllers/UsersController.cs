69437368f5ad202e23d2349ec9ef7114be1f0785
Dan23P <149869797+Dan23P@users.noreply.github.com>
2024-08-25 21:44:28 +0200

    Add project files.

---
+++ Homework 1/Homework1/Homework1/Controllers/UsersController.cs
@@ -0,0 +1,47 @@
+﻿using Homework1.Database;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Homework1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<string>> Get()
+        {
+            return Ok(StaticDb.Users);
+        }
+
+
+        [HttpGet("{index}")]
+        public ActionResult<string> GetByIndex(int index)
+        {
+            try
+            {
+                if (index < 0)
+                {
+                    return BadRequest("The index has negative value");
+                }
+                if (index >= StaticDb.Users.Count)
+                {
+                    return NotFound("The User was not found");
+                }
+
+                return Ok(StaticDb.Users[index]);
+            }
+            catch (Exception ex)
+            {
+                
+                 return StatusCode(StatusCodes.Status500InternalServerError, "An error occured. Please contact your administrator");
+                
+            }
+        }
+    }
+
+  
+    
+        
+    
+}
---
+++ Homework 1/Homework1/Homework1/Database/StaticDb.cs
@@ -0,0 +1,13 @@
+﻿namespace Homework1.Database
+{
+    public static class StaticDb
+    {
+        public static List<string> Users = new List<string>()
+        {
+            "Bob",
+            "Bobski",
+            "Boris",
+            "Bojan"
+        };
+    }
+}
